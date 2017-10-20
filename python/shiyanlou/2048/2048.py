97d26acf33191282c6e8b762bbe53b38e072770b
Arckman <fengrj1989@gmail.com>
2017-10-20 17:23:25 +0800

    coding python/shiyanlou/2048-10.20

--- python/shiyanlou/2048/2048.py
+++ python/shiyanlou/2048/2048.py
@@ -1,5 +1,21 @@
-from curses import *
+import curses
 from random import *
+from collections import defaultdict
+
+'''
+FiniteStateMachine:
+    +--------restart--------- [Win] -----exit------+
+    |                           ^                  |
+    |                           |                  |
+    |                          win                 |
+    V                           |                  V
+  [Init] <---init,restart---> [Game] ---exit---> [Exit]
+    ^                           |                  ^
+    |                        gameover              |
+    |                           |                  |
+    |                           V                  |
+    +--------restart------- [Gameover] ---exit-----+
+'''
 
 letter_codes = [ord(ch) for ch in 'WASDRQwasdrq']
 actions = ['Up', 'Left', 'Down', 'Right', 'Restart', 'Exit']
@@ -15,7 +31,7 @@ class GameField:
         self.reset()
     
     def reset(self):
-        self.heighest_score=self.score if self.score>self.heighest_score
+        if self.score > self.heighest_score:self.heighest_score=self.score 
         self.score=0
         self.field=[[0 for i in range(self.width)] for i in range(self.height)]
         self.spawn()
@@ -69,7 +85,7 @@ class GameField:
     def spawn(self):
         new_element=4 if randrange(100)>89 else 2
         (i,j)=choice([(i,j) for i in range(self.width) for j in range(self.height) if self.field[i][j]==0])
-        self[i][j]=new_element
+        self.field[i][j]=new_element
     
     def is_win(self):
         return any(any(i >= self.win_score for i in row) for row in self.field)
@@ -88,20 +104,93 @@ class GameField:
                 return False
             return any(unit_left_is_possible(i) for i in range(len(row)-1))
         check={}#TODO:deep copy?
-        check['left']=lambda field:any(move_left_is_possible(row) for row in field)
-        check['right']=lambda field:any(check['left'](GameField.__invert(field)))
-        check['up']=lambda field:any(check['left'](GameField.__transpose(field)))
-        check['down']=lambda field:any(check['right'](GameField.__transpose(field)))
+        check['Left']=lambda field:any(move_left_is_possible(row) for row in field)
+        check['Right']=lambda field:any(check['Left'](GameField.__invert(field)))
+        check['Up']=lambda field:any(check['Left'](GameField.__transpose(field)))
+        check['Down']=lambda field:any(check['Right'](GameField.__transpose(field)))
+        # print(direction)
         if direction in actions:
             return check[direction](self.field)
         else:
             return False
-    
 
+    def draw(self,screen):
+        help_string1='(W)Up (S)Down (A)Left (D)Right'    
+        help_string2='\t(R)Restart (Q)Exit'
+        gameover_string = '\tGAME OVER'
+        win_string = '\tYOU WIN!'
+        def cast(string):
+            screen.addstr(string+'\n')
+        def draw_hor_separator():
+            line='+'+('+--------'*self.width+'+')[1:]
+            separator=defaultdict(lambda:line)#lamda:line is the default_factory
+            if not hasattr(draw_hor_separator,"counter"):
+                draw_hor_separator.counter=0
+            cast(separator[draw_hor_separator.counter])
+            draw_hor_separator.counter+=1
+        def draw_row(row):
+            cast(''.join('|{: ^5} '.format(num) if num > 0 else '|      ' for num in row) + '|')
+        screen.clear()
+        cast('SCORE: '+str(self.score))
+        if 0!=self.heighest_score:
+            cast('HIGHSCORE: ' + str(self.highscore))
+        for row in self.field:
+            draw_hor_separator()
+            draw_row(row)
+        draw_hor_separator()
+        if self.is_win():
+            cast(win_string)
+        else:
+            if self.is_gameover():
+                cast(gameover_string)
+            else:
+                cast(help_string1)
+        cast(help_string2)
 
-            
-
-def main():
+def main(stdscr):
     def init():
         #init state
-        game_field.
+        game_field.reset()
+        return 'Game'
+    
+    def get_user_action(keyboard):
+        char='N'
+        while char not in actions_dict:
+            char=keyboard.getch()
+        return actions_dict[char]
+
+    def not_game(state):
+        game_field.draw(stdscr)
+        action=get_user_action(stdscr)
+        responses=defaultdict(lambda:state)
+        responses['Restart'],responses['Exit']='Init','Exit'
+        return responses[action]
+
+    def game():
+        game_field.draw(stdscr)
+        action=get_user_action(stdscr)
+        if action=='Restart':
+            return 'Init'
+        if action=='Exit':
+            return 'Exit'
+        if game_field.move(action):
+            if game_field.is_win():
+                return 'Win'
+            if game_field.is_gameover():
+                return 'Gameover'
+        return 'Game'
+    
+    state_actions={
+        'Init':init,
+        'Win':lambda:not_game('Win'),
+        'Game_over':lambda:not_game('Gameover'),
+        'Game':game
+    }
+    curses.use_default_colors()
+    game_field=GameField(win=32)
+
+    state='Init'
+    while state!='Exit':
+        state=state_actions[state]()
+
+curses.wrapper(main)

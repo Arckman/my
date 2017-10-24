20db0526aa9477aa523294e4d45b6c2a90b36d27
Arckman <fengrj1989@gmail.com>
2017-10-24 17:13:33 +0800

    python/shiyanlou/feijigame_init

---
+++ python/shiyanlou/feijigame/pygame_test/test_moveit.py
@@ -0,0 +1,8 @@
+import pygame
+
+terrain1="t1"
+terrain2="t2"
+pygame.init()
+
+background = [terrain1, terrain1, terrain2, terrain2, terrain2, terrain1]
+screen=pygame.display.create_graphics_screen()

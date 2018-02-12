5b7851ef343e9279498fb4f9d391609c8eb6129d
Arckman <fengrj1989@gmail.com>
2018-02-12 09:35:17 +0800

    scrapy dmoz xpath

--- python/scrapy/tutorial/tutorial/spiders/dmoz_spider.py
+++ python/scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -8,6 +8,8 @@ class DmozSpider(scrapy.Spider):
         	"http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
 	]
 	def parse(self,response):
-		filename=response.url.split("/")[-2]+'.html'
-		with open(filename,'wb') as f:
-			f.write(response.body)
+		for sel in response.xpath('//ul/li'):
+			title=sel.xpath('a/text()').extract()
+			link=sel.xpath('a/@href').extract()
+			desc=sel.xpath('text()').extract()
+			print(title,link,desc)
